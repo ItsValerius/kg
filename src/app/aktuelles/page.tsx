18ac90cd43f654658502ce2ee77592f942bee8a6
valentin kueck <kueck.valentin@googlemail.com>
2024-05-20 22:39:30 +0200

    adjust styling

--- src/app/aktuelles/page.tsx
+++ src/app/aktuelles/page.tsx
@@ -10,6 +10,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Suspense } from "react";
 import FallbackCard from "@/components/postPage/fallbackCard";
+import { H2 } from "@/components/typography/h2";
 
 export const metadata: Metadata = {
   title: "Aktuelles",
@@ -23,40 +24,27 @@ const AktuellesPage = async () => {
 
   return (
     <main>
-      <div className="flex flex-col gap-12 py-12 md:py-16 lg:py-24">
-        <section className=" flex flex-col items-center gap-6 text-center">
-          <div className="flex flex-col items-center gap-3">
-            <Link className="flex items-center space-x-2" href="/">
-              <Image
-                src="https://knallkoepp-golkrath.de/s/misc/logo.png"
-                width={64}
-                height={64}
-                alt="logo"
-              />
-            </Link>
-            <H1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl">
-              KG Knallköpp Golkrath News
-            </H1>
+      <section className="px-4 py-12 md:px-6">
+        <div className="mx-auto max-w-5xl space-y-8">
+          <div className="space-y-2 text-center">
+            <H2>Aktuelles</H2>
+            <P>Erfahren Sie aktuelles aus unserem Verein.</P>
           </div>
-          <P className="max-w-[700px] text-gray-500 dark:text-gray-400 md:text-xl">
-            Stay up to date with the latest news and events from our local
-            community club.
-          </P>
-        </section>
-        <div className="container grid grid-cols-1 gap-8 lg:grid-cols-[1fr_300px]">
-          <div className="grid gap-8">
-            {posts.map((post) => {
-              return <PostCard post={post} key={post.id} />;
-            })}
-          </div>
-          <div className="grid space-y-8">
-            <Suspense fallback={<FallbackCard />}>
-              <CategoriesCard categories={categories} />
-            </Suspense>
-            <SearchCard />
+          <div className="container grid grid-cols-1 gap-8 lg:grid-cols-[1fr_300px]">
+            <div className="grid gap-8">
+              {posts.map((post) => {
+                return <PostCard post={post} key={post.id} />;
+              })}
+            </div>
+            <div className="grid space-y-8">
+              <Suspense fallback={<FallbackCard />}>
+                <CategoriesCard categories={categories} />
+              </Suspense>
+              <SearchCard />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </main>
   );
 };
