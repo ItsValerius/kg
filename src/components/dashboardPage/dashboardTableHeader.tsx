4480b7eb2979c6d4a7dbd8327a8f2adf4a57d320
valentin kueck <kueck.valentin@googlemail.com>
2024-05-28 19:24:01 +0200

    remove searchbar

--- src/components/dashboardPage/dashboardTableHeader.tsx
+++ src/components/dashboardPage/dashboardTableHeader.tsx
@@ -21,7 +21,7 @@ import {
   DropdownMenuTrigger,
 } from "../ui/dropdown-menu";
 import { Input } from "../ui/input";
-import { SelectAccount } from "@/server/db/schema";
+import type { SelectAccount } from "@/server/db/schema";
 
 const DashboardTableHeader = ({
   currentAccount,
@@ -32,7 +32,7 @@ const DashboardTableHeader = ({
   const pathnames = pathname.split("/").filter((name) => !!name);
 
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6 justify-between">
       <Breadcrumb className="hidden md:flex">
         <BreadcrumbList>
           {pathnames.map((name, i) => {
@@ -64,14 +64,7 @@ const DashboardTableHeader = ({
           })}
         </BreadcrumbList>
       </Breadcrumb>
-      <div className="relative ml-auto flex-1 md:grow-0">
-        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
-        />
-      </div>
+
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button
