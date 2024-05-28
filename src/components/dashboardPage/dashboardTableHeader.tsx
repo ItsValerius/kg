1d03fbd6d9e6612b4db7a3d196c0165b9bfa3e9f
valentin kueck <kueck.valentin@googlemail.com>
2024-05-28 19:23:15 +0200

    readd component

---
+++ src/components/dashboardPage/dashboardTableHeader.tsx
@@ -0,0 +1,104 @@
+"use client";
+import { Search } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "../ui/breadcrumb";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Input } from "../ui/input";
+import { SelectAccount } from "@/server/db/schema";
+
+const DashboardTableHeader = ({
+  currentAccount,
+}: {
+  currentAccount: SelectAccount;
+}) => {
+  const pathname = usePathname();
+  const pathnames = pathname.split("/").filter((name) => !!name);
+
+  return (
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 py-2 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+      <Breadcrumb className="hidden md:flex">
+        <BreadcrumbList>
+          {pathnames.map((name, i) => {
+            return (
+              <React.Fragment key={i}>
+                <BreadcrumbItem
+                  className={
+                    i + 1 === pathnames.length
+                      ? "text-xl text-foreground"
+                      : "text-muted-foreground"
+                  }
+                >
+                  <BreadcrumbLink asChild>
+                    <Link
+                      href={
+                        i === 0
+                          ? `/${name}`
+                          : `/${pathnames.slice(0, i + 1).join("/")}`
+                      }
+                      className="capitalize"
+                    >
+                      {name}
+                    </Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+                {i + 1 < pathnames.length && <BreadcrumbSeparator />}
+              </React.Fragment>
+            );
+          })}
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="relative ml-auto flex-1 md:grow-0">
+        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+        <Input
+          type="search"
+          placeholder="Search..."
+          className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+        />
+      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="outline"
+            size="icon"
+            className="overflow-hidden rounded-full"
+          >
+            <Image
+              src={currentAccount.imageUrl}
+              width={36}
+              height={36}
+              alt="Avatar"
+              className="overflow-hidden rounded-full"
+            />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Settings</DropdownMenuItem>
+          <DropdownMenuItem>Support</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Logout</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </header>
+  );
+};
+
+export default DashboardTableHeader;
