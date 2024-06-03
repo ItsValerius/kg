b30f06a10e1756f087068202046e7e21593d54cf
valentin kueck <kueck.valentin@googlemail.com>
2024-06-03 17:30:03 +0200

    set correct variable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,7 @@ import NewsSkeleton from "@/components/homepage/newsSkeleton";
 import UeberunsSection from "@/components/homepage/ueberunsSection";
 import { Suspense } from "react";
 
-export const config = {
-  maxDuration: 30,
-};
+export const maxDuration = 30;
 
 export default function Home() {
   return (
